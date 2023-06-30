be20a030c73dd5caafec99ca02712c5d1a7578b9
MrPushistik <345w2011@mail.ru>
2023-06-30 19:32:38 +0400

    user controller updated

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,10 +1,16 @@
-const {User} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const {User} = require('../models/models');
+const {Credential} = require("../models/models");
+const ApiError = require('../error/ApiError');
+
 class UserController{
     async registration(req,res,next){
         try{
-            const {surname,name,patronymic,post,placeWorkOrStudy,phone,email,dateCreation} = req.body
-            const user = await User.create({surname,name,patronymic,post,placeWorkOrStudy,phone,email,dateCreation})
+            const {login,password,confirmPassword,role,surname,name,patronymic,post,placeWorkOrStudy,phone,email,dateCreation} = req.body
+            if (password!=confirmPassword){
+                next(ApiError.badRequest("Неверный пароль"));
+            }
+            const credential = await Credential.create({login,password,role})
+            const user = await User.create({surname,name,patronymic,post,placeWorkOrStudy,phone,email,dateCreation,credentialId:credential.id})
             return res.json(user)
         }
         catch(e){
@@ -12,16 +18,33 @@ class UserController{
         }
     }
 
-    async login(req,res){
-        
+    async login(req,res,next){
+        try{
+            const {login,password,role} = req.body;
+            const credential = await Credential.findOne({where: {login: login}});
+            if (password!=credential.password){
+                next(ApiError.badRequest("Неверный пароль"));
+            }
+            const user = await User.findOne({where: {credentialId: credential.id}});
+            return res.json(user);
+        }
+        catch(e){
+            next(ApiError.badRequest(e.message))
+        }
     }
 
     async check(req,res,next){
-        const {id} = req.query
-        if (!id){
-            return next(ApiError.badRequest('Не задан ID'))
+        try{
+            const {id} = req.query
+            if (!id){
+                return next(ApiError.badRequest('Не задан ID'))
+            }
+            return res.json(id)
         }
-        res.json(id)
+        catch(e){
+            next(ApiError.badRequest("Неверный формат данных"));
+        }
+        
     }
 }
 
