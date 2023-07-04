a29ebc08b9ef9f1ed92e9369079acdce0af5959c
MrPushistik <345w2011@mail.ru>
2023-07-04 16:24:33 +0400

    security added

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,17 +1,44 @@
 const {User} = require('../models/models');
 const {Credential} = require("../models/models");
 const ApiError = require('../error/ApiError');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const generateJwt = (id, login, role) => {
+    return jwt.sign(
+        {id,login,role},
+        process.env.SECRET_KEY,
+        {expiresIn:'24h'}
+    )
+}
 
 class UserController{
     async registration(req,res,next){
         try{
             const {login,password,confirmPassword,role,surname,name,patronymic,post,placeWorkOrStudy,phone,email} = req.body //-dateCreation
-            if (password!=confirmPassword){
-                next(ApiError.badRequest("Неверный пароль"));
+            if (!login || !confirmPassword || !surname || !patronymic || !placeWorkOrStudy || !email){
+                return next(ApiError.badRequest("Неверный формат данных"));
             }
-            const credential = await Credential.create({login,password,role})
+            if (password!=confirmPassword){
+                return next(ApiError.badRequest("Неверный пароль"));
+            }
+            const candidate1 = await Credential.findOne({where:{login}})
+            if (candidate1){
+                return next(ApiError.badRequest("Пользователь с таким login уже существует"));
+            }
+            const candidate2 = await User.findOne({where:{email}})
+            if (candidate2){
+                return next(ApiError.badRequest("Пользователь с таким email уже существует"));
+            }
+            const candidate3 = await User.findOne({where:{phone}})
+            if (candidate3){
+                return next(ApiError.badRequest("Пользователь с таким phone уже существует"));
+            }
+            const hashPassword = await bcrypt.hash(password,5)
+            const credential = await Credential.create({login,password:hashPassword,role})
             const user = await User.create({surname,name,patronymic,post,placeWorkOrStudy,phone,email,credentialId:credential.id})
-            return res.json(user)
+            const token = generateJwt(credential.id,credential.login,credential.role)
+            return res.json({token})
         }
         catch(e){
             next(ApiError.badRequest(e.message))
@@ -20,13 +47,17 @@ class UserController{
 
     async login(req,res,next){
         try{
-            const {login,password,role} = req.body;
-            const credential = await Credential.findOne({where: {login: login}});
-            if (password!=credential.password){
-                next(ApiError.badRequest("Неверный пароль"));
+            const {login,password} = req.body;
+            const credential = await Credential.findOne({where: {login}});
+            if (!credential){
+                return next(ApiError.badRequest("Пользователь не найден"));
             }
-            const user = await User.findOne({where: {credentialId: credential.id}});
-            return res.json(user);
+            let comparePassword = bcrypt.compareSync(password,credential.password)
+            if (!comparePassword){
+                next(ApiError.badRequest("Неверный пароль или логин"));
+            }
+            const token = generateJwt(credential.id,credential.login,credential.role)
+            return res.json({token})
         }
         catch(e){
             next(ApiError.badRequest(e.message))
@@ -34,17 +65,8 @@ class UserController{
     }
 
     async check(req,res,next){
-        try{
-            const {id} = req.query
-            if (!id){
-                return next(ApiError.badRequest('Не задан ID'))
-            }
-            return res.json(id)
-        }
-        catch(e){
-            next(ApiError.badRequest("Неверный формат данных"));
-        }
-        
+        const token = generateJwt(req.user.id, req.user.login, req.user.role)
+        return res.json({token})
     }
 
     async delete(req,res,next){
